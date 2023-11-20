9b5269fe8eb9c1da87a4d77cffa474f2bd47273c
WimObiwan <wim@obiwan.be>
2023-11-20 23:26:18 +0100

    Site: Fixed version string

--- Site/Pages/Info.cshtml.cs
+++ Site/Pages/Info.cshtml.cs
@@ -19,8 +19,9 @@ public class Info : PageModel
     {
         RequestHost = HttpContext.Request.Host.ToString();
         Server = Environment.MachineName;
-        Version = Assembly.GetEntryAssembly()
-            ?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+        Version = ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.Commits 
+                  + "-" + ThisAssembly.Git.Branch + "+" + ThisAssembly.Git.Commit
+                  + "  " + ThisAssembly.Git.CommitDate;
         OsVersion = RuntimeInformation.OSDescription;
         DotNetVersion = RuntimeInformation.FrameworkDescription;
         UserAuthId = HttpContext.User.Identity?.Name;
