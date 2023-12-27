992697a9ce9f4f5ff0adc807268b084a652fd654
WimObiwan <wim@obiwan.be>
2023-12-27 11:20:40 +0100

    Initial Account page

---
+++ Site/Pages/Account.cshtml.cs
@@ -0,0 +1,56 @@
+using System.Globalization;
+using System.Text;
+using Core.Commands;
+using Core.Entities;
+using Core.Queries;
+using Core.Util;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Site.Utilities;
+
+namespace Site.Pages;
+
+public class Account : PageModel
+{
+    private readonly IMediator _mediator;
+    private readonly IUserInfo _userInfo;
+
+    public Core.Entities.Account? AccountEntity { get; set; }
+    public IList<MeasurementEx>? Measurements { get; set; }
+
+    public Account(IMediator mediator, IUserInfo userInfo)
+    {
+        _mediator = mediator;
+        _userInfo = userInfo;
+    }
+
+    public async Task OnGet(string accountLink)
+    {
+        AccountEntity = await _mediator.Send(new AccountByLinkQuery
+        {
+            Link = accountLink
+        });
+
+        if (AccountEntity == null)
+        {
+            //...
+        }
+        else
+        {
+            Measurements = (await Task.WhenAll(AccountEntity.AccountSensors.Select(async accountSensor =>
+            {
+                var lastMeasurement = await _mediator.Send(new LastMeasurementQuery
+                    { DevEui = accountSensor.Sensor.DevEui });
+                if (lastMeasurement != null)
+                    return new MeasurementEx(lastMeasurement, accountSensor);
+                else
+                    return null;
+            })))
+                .Where(m => m != null)
+                .Cast<MeasurementEx>()
+                .ToList();
+        }
+
+    }
+}
