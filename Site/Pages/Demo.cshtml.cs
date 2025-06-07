ec13c12a2682b72c68e22c37b20ae11f86394284
WimObiwan <wim@obiwan.be>
2025-06-07 11:47:55 +0200

    Generalized Demo path

--- Site/Pages/Demo.cshtml.cs
+++ Site/Pages/Demo.cshtml.cs
@@ -5,11 +5,20 @@ namespace Site.Pages;
 
 public class Demo : PageModel
 {
+    private readonly IConfiguration _configuration;
+
+    public Demo(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     public IActionResult OnGet()
     {
+        return Redirect(_configuration["DemoPath"] ?? "/a/demo/s/f2y616afaEA");
+
         // Buiten
-        return Redirect($"/a/demo/s/f2y616afaEA");
+        //return Redirect("/a/demo/s/f2y616afaEA");
         // Garage
-        // return Redirect($"/a/demo/s/ter0TfgBG58");
+        //return Redirect($"/a/demo/s/ter0TfgBG58");
     }
 }
