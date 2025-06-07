bb047335d2441ee8a2161a2b90f5b8b6c79e48fa
WimObiwan <wim@obiwan.be>
2025-06-07 11:50:54 +0200

    Optionally load Account info with SensorByLinkQuery

--- Core/Queries/SensorByLinkQueryHandler.cs
+++ Core/Queries/SensorByLinkQueryHandler.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.Eventing.Reader;
 using Core.Entities;
 using Core.Repositories;
 using MediatR;
@@ -8,6 +9,7 @@ namespace Core.Queries;
 public record SensorByLinkQuery : IRequest<Sensor?>
 {
     public required string SensorLink { get; init; }
+    public bool IncludeAccount { get; init; } = false;
 }
 
 public class SensorByLinkQueryHandler : IRequestHandler<SensorByLinkQuery, Sensor?>
@@ -21,8 +23,18 @@ public class SensorByLinkQueryHandler : IRequestHandler<SensorByLinkQuery, Senso
 
     public async Task<Sensor?> Handle(SensorByLinkQuery request, CancellationToken cancellationToken)
     {
-        return await _dbContext.Sensors
-            .Where(s => s.Link == request.SensorLink || s.DevEui == request.SensorLink)
+        IQueryable<Sensor> query =
+            _dbContext.Sensors
+                .Where(s => s.Link == request.SensorLink || s.DevEui == request.SensorLink);
+
+        if (request.IncludeAccount)
+        {
+            query = query
+                .Include(s => s.AccountSensors)
+                .ThenInclude(@as => @as.Account);
+        }
+
+        return await query
             .SingleOrDefaultAsync(cancellationToken);
     }
 }
