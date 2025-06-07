244fa3e222c558668c9bd1baed28ca6e6d696651
WimObiwan <wim@obiwan.be>
2025-06-07 12:25:27 +0200

    Help: Fixed lookup by 5G ID

--- Site/Pages/Help.cshtml.cs
+++ Site/Pages/Help.cshtml.cs
@@ -27,13 +27,29 @@ public class Help : PageModel
         return Page();
     }
 
+    private async Task<Core.Entities.Account?> GetAccountByEmail(string email)
+    {
+        return await _mediator.Send(new AccountByEmailQuery() { Email = email });
+    } 
+
+    private async Task<Core.Entities.Sensor?> GetSensorById(string id)
+    {
+        return await _mediator.Send(new SensorByLinkQuery() { SensorLink = id, IncludeAccount = true });
+    } 
+
     public async Task<IActionResult> OnPost(string id)
     {
         string? restPath = null;
-        var account = await _mediator.Send(new AccountByEmailQuery() { Email = id });
+        var account = await GetAccountByEmail(id);
         if (account == null)
         {
-            var sensor = await _mediator.Send(new SensorByLinkQuery() { SensorLink = id, IncludeAccount = true });
+            var sensor = await GetSensorById(id);
+            if (sensor == null)
+            {
+                // 5G sensors have an id starting with 'F', which is not printed on the sensor sticker.
+                sensor = await GetSensorById('F' + id);
+            }
+
             if (sensor != null)
             {
                 var accountSensors = sensor.AccountSensors
