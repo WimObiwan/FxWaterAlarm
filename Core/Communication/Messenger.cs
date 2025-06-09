62ad8a06ed4b17f497aa61625d55efd3b63f3b17
WimObiwan <wim@obiwan.be>
2025-06-09 21:01:23 +0200

    Generalized mail logic, simplified html

--- Core/Communication/Messenger.cs
+++ Core/Communication/Messenger.cs
@@ -50,28 +50,47 @@ public class Messenger : IMessenger
     
     public async Task SendAuthenticationMailAsync(string emailAddress, string url, string code)
     {
-        string body = await File.ReadAllTextAsync(GetContentPath("authentication-mail.html"));
-        body = body
+        string subject = "WaterAlarm Log-in code";
+
+        string contents = """
+            <h1>Uw log-in code<br>voor wateralarm.be</h1>
+            <div class="code">{{LOGINCODE}}</div>
+            <div class="description">
+              Gebruik deze log-in code om aan te melden op wateralarm.be,<br>
+              of klik op de knop hieronder om meteen aan te melden.
+            </div>
+            <a href="{{LOGINURL}}" class="btn">Inloggen</a>
+            """;
+
+        contents = contents
             .Replace("{{LOGINURL}}", url)
             .Replace("{{LOGINCODE}}", code);
 
-        LinkedResource[] linkedResources =
-        {
-            new LinkedResource(GetContentPath("images/wateralarm.png"))
-                { ContentId = "images-wateralarm.png",  ContentType = new ContentType("image/png") },
-            new LinkedResource(GetContentPath("images/check-icon.png"))
-                { ContentId = "images-check-icon.png",  ContentType = new ContentType("image/png") },
-        };
-
-        await SendMailAsync(emailAddress, "WaterAlarm.be e-mail verificatie", body, linkedResources);
+        await SendMailAsync(emailAddress, subject, contents);
     }
 
     public async Task SendAlertMailAsync(string emailAddress, string url, string? accountSensorName, string alertMessage, string shortAlertMessage)
     {
-        string subject = "WaterAlarm.be melding - {{ACCOUNTSENSORNAME}} - {{SHORTALERTMESSAGE}}";
-        string body = await File.ReadAllTextAsync(GetContentPath("alert-mail.html"));
+        string subject = "WaterAlarm Melding - {{ACCOUNTSENSORNAME}} - {{SHORTALERTMESSAGE}}";
 
-        body = body
+        string content = """
+            <h1>Melding voor uw sensor<br/><strong>{{ACCOUNTSENSORNAME}}</strong></h1>
+            <div class="description">
+              Er is een melding voor uw sensor:<br>
+              <strong>{{ALERTMESSAGE}}</strong>
+            </div>
+            <div class="description">
+              <span>Bekijk de details van uw sensor met de knop hieronder.</span>
+            </div>
+            <a href="{{URL}}" class="btn">Inloggen</a>
+            <p><small>
+              Als je deze meldingen niet meer wilt ontvangen, dan kun je deze zelf afzetten in de instellingen van je sensor.
+              Of je kunt ons vragen om dit te desactiveren als antwoord op deze melding.
+              Duidt deze mail niet aan als SPAM of ONGEWENST, om te vermijden dat andere ontvangers de mails niet meer ontvangen.
+            </small></p>
+            """;
+
+        content = content
             .Replace("{{URL}}", url)
             .Replace("{{ACCOUNTSENSORNAME}}", accountSensorName)
             .Replace("{{ALERTMESSAGE}}", alertMessage);
@@ -79,35 +98,35 @@ public class Messenger : IMessenger
             .Replace("{{ACCOUNTSENSORNAME}}", accountSensorName)
             .Replace("{{SHORTALERTMESSAGE}}", shortAlertMessage);
 
-        LinkedResource[] linkedResources =
-        {
-            new LinkedResource(GetContentPath("images/wateralarm.png"))
-                { ContentId = "images-wateralarm.png",  ContentType = new ContentType("image/png") },
-            new LinkedResource(GetContentPath("images/warning.png"))
-                { ContentId = "images-warning-icon.png",  ContentType = new ContentType("image/png") },
-        };
-
-        string emailAddressToUse;
-        if (_messengerOptions.OverruleAlertDestination is {} overruleDestination && !string.IsNullOrEmpty(overruleDestination))
-        {
-            _logger.LogWarning("Mail alert to real address {emailAddress} overruled by {overruleDestination}", emailAddress, overruleDestination);
-            emailAddressToUse = overruleDestination;
-        }
-        else
-        {
-            emailAddressToUse = emailAddress;
-        }
-
-        await SendMailAsync(emailAddressToUse, subject, body, linkedResources);
+        await SendMailAsync(emailAddress, subject, content);
     }
 
     public async Task SendLinkMailAsync(string emailAddress, string url)
     {
-        string subject = "WaterAlarm.be Link";
-        string body = await File.ReadAllTextAsync(GetContentPath("help-mail.html"));
+        string subject = "WaterAlarm Link";
 
-        body = body
+        string contents = """
+    <div class="content">
+      <h1>Uw link<br>voor WaterAlarm</h1>
+      <div class="description">
+        Je vroeg om jouw unieke sensor of account link opnieuw te ontvangen:<br/>
+        <a href="{{URL}}" title="link">{{URL}}</a>
+      </div>
+      <a href="{{URL}}" class="btn">Uw sensor</a>
+    </div>
+""";
+
+        contents = contents
             .Replace("{{URL}}", url);
+
+        await SendMailAsync(emailAddress, subject, contents);
+    }
+
+    public async Task SendMailAsync(string emailAddress, string subject, string contents)
+    {
+        string body = await File.ReadAllTextAsync(GetContentPath("mail.html"));
+        body = body
+            .Replace("{{CONTENTS}}", contents);
 
         LinkedResource[] linkedResources =
         {
@@ -115,23 +134,18 @@ public class Messenger : IMessenger
                 { ContentId = "images-wateralarm.png",  ContentType = new ContentType("image/png") },
         };
 
-        string emailAddressToUse;
-        if (_messengerOptions.OverruleAlertDestination is {} overruleDestination && !string.IsNullOrEmpty(overruleDestination))
-        {
-            _logger.LogWarning("Mail link to real address {emailAddress} overruled by {overruleDestination}", emailAddress, overruleDestination);
-            emailAddressToUse = overruleDestination;
-        }
-        else
-        {
-            emailAddressToUse = emailAddress;
-        }
-
-        await SendMailAsync(emailAddressToUse, subject, body, linkedResources);
+        await SendMailAsync(emailAddress, subject, body, linkedResources);
     }
 
     private async Task SendMailAsync(string emailAddress, string subject, string body, IList<LinkedResource> linkedResources)
     {
         _logger.LogInformation("Sending mail to {destination}, Subject: {subject}", emailAddress, subject);
+
+        if (_messengerOptions.OverruleAlertDestination is {} overruleDestination && !string.IsNullOrEmpty(overruleDestination))
+        {
+            _logger.LogWarning("Mail link to real address {emailAddress} overruled by {overruleDestination}", emailAddress, overruleDestination);
+            emailAddress = overruleDestination;
+        }
 
         var smtpClient = new SmtpClient(_messengerOptions.SmtpServer)
         {
