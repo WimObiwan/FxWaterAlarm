544e195d2b92c6478e99294f432590e29a269693
WimObiwan <wim@obiwan.be>
2026-02-07 00:12:11 +0100

    Added deploy script

---
+++ scripts/deploy-wateralarm-prd-template.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+TARGET_SERVER="someserver"
+"$SCRIPT_DIR/deploy.sh" "net10.0" "$TARGET_SERVER" "/var/www/www.site" "kestrel-site" "/opt/site-cli" "/opt/site-admin"
